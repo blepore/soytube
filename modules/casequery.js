f3a6683c578ba7fa6b771788a3c0503d205c4c87
blepore <brianwlepore@gmail.com>
2017-04-26 15:52:11 -0400

    Update casequery.js

--- modules/casequery.js
+++ modules/casequery.js
@@ -15,29 +15,20 @@ exports.execute = (req, res) => {
     let slackUserId = req.body.user_id,
         oauthObj = auth.getOAuthObject(slackUserId),
         limit = req.body.text,
-        q = "SELECT Id, Name, Amount, Probability, StageName, CloseDate FROM Opportunity where isClosed=false ORDER BY amount DESC LIMIT " + limit;
+        q = "select id from case where CaseNumber = " + limit;
 
     if (!limit || limit=="") limit = 5;
 
     force.query(oauthObj, q)
         .then(data => {
-            let opportunities = JSON.parse(data).records;
-            if (opportunities && opportunities.length > 0) {
+            let cases = JSON.parse(data).records;
+            if (cases && cases.length > 0) {
                 let attachments = [];
-                opportunities.forEach(function (opportunity) {
+                cases.forEach(function (case) {
                     let fields = [];
-                    fields.push({title: "Opportunity", value: opportunity.Name, short: true});
-                    fields.push({title: "Stage", value: opportunity.StageName, short: true});
-                    fields.push({
-                        title: "Amount",
-                        value: new Intl.NumberFormat('en-US', {
-                            style: 'currency',
-                            currency: 'USD'
-                        }).format(opportunity.Amount),
-                        short: true
-                    });
-                    fields.push({title: "Probability", value: opportunity.Probability + "%", short: true});
-                    fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + opportunity.Id, short:false});
+                    fields.push({title: "Caseid", value: case.id, short: true});
+                   
+                    fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + case.Id, short:false});
                     attachments.push({
                         color: "#FCB95B",
                         fields: fields
